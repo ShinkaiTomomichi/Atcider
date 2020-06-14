9aaff385e30e2cd623d9ca7417dbf1bfd9a3ca63
shinkaitomomichi <shinkaitomomichi@shinkaitomomichinoMacBook-Pro.local>
2020-06-14 23:14:35 +0900

    ABC169 sample commit

---
+++ ABC169/ABC_169A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>  // istringstream
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{   
+    int a, b;
+    cin >> a >> b;
+
+    cout << a*b << endl;
+    return 0;
+}
